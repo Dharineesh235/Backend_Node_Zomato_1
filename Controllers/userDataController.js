28cdd8d8ad84da7958812367a2c03f5f4f8f9960
Dharineesh235 <dharineesh235@gmail.com>
2023-05-11 16:46:47 +0530

    added account creation message

--- Controllers/userDataController.js
+++ Controllers/userDataController.js
@@ -3,7 +3,10 @@ const UserData=require('../Models/userDataModel');
 exports.postUserData=(req,res)=>{
     console.log(req.body);
     UserData.insertMany({user_name:req.body.name, user_email:req.body.email, user_password:req.body.password}).then((result)=>{
-        res.send(result);
+        res.send({
+            message: "Account Created",
+            data : result
+        });
     })
 }
 exports.getAllUsers=(req,res)=>{
