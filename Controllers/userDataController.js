52dab31fb0637a558e861107e809ae09fb934f27
Dharineesh235 <dharineesh235@gmail.com>
2023-05-11 21:13:59 +0530

    console option for postUser

--- Controllers/userDataController.js
+++ Controllers/userDataController.js
@@ -2,12 +2,13 @@ const UserData=require('../Models/userDataModel');
 
 exports.postUserData=(req,res)=>{
     console.log(req.body);
-    UserData.insertMany({user_name:req.body.name, user_email:req.body.email, user_password:req.body.password}).then((result)=>{
-        res.send({
-            message: "Account Created",
-            data : result
-        });
-    })
+    res.send(req.body)
+    // UserData.insertMany({user_name:req.body.name, user_email:req.body.email, user_password:req.body.password}).then((result)=>{
+    //     res.send({
+    //         message: "Account Created",
+    //         data : result
+    //     });
+    // })
 }
 exports.getAllUsers=(req,res)=>{
     console.log(req.body);
