28c3fd0612447327bd2262d10a273b616ad0ce4b
Dharineesh235 <dharineesh235@gmail.com>
2023-05-12 10:47:51 +0530

    Bugs fixed

--- Controllers/userDataController.js
+++ Controllers/userDataController.js
@@ -29,24 +29,22 @@ exports.getAllUsers=(req,res)=>{
     UserData.find(filter_email).then((result_mail)=>{
         console.log(result_mail);
         if(result_mail.length > 0){
-            UserData.find(filter_pass).then(result_pass=>{
-                console.log(result_pass);
-                if(result_pass.length > 0){
-                    res.send({
-                    data : [{
-                                user_email:result_pass[0].user_email,
-                                user_name : result_pass[0].user_name
-                            }],
-                    message : "Log in Successful",
-                    })
-                }else{
-                    res.send({
-                        data : [],
-                        message : "Incorrect Password !"
-                    })
-                }
-            })
-        }else{
+
+            if(result_mail[0].user_password == req.body.userPassWord){
+                res.send({
+                data : result_mail,
+                message : "Log in Successful",
+                })
+            }
+            else{
+                res.send({
+                    data : [],
+                    message : "Incorrect Password !"
+                })
+            }
+        }
+       else
+        {
         res.send({
             data : [],
             message : "Invalid Email !",
